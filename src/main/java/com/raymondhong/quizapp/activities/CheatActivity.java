af6e3a7e08f8734b7aa07f8a264c8a31d50599cd
rhong612 <rhong612@gmail.com>
2016-08-06 17:02:10 -0700

    Added cheat button

---
+++ src/main/java/com/raymondhong/quizapp/activities/CheatActivity.java
@@ -0,0 +1,13 @@
+package com.raymondhong.quizapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
